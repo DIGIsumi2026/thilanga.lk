a6d61a366771be39a44a921f4585c90d71368623
RK17904 <ravindu.kaveesha2001@gmail.com>
2026-09-18 16:03:57 +0530

    delete-files

--- frontend/src/assets/imageAssets.ts
+++ frontend/src/assets/imageAssets.ts
@@ -161,26 +161,6 @@ import sumathiAwardsImage8 from '../assets/images/public-relations/social-servic
 import contactHero  from '../assets/images/contact/hero.png';
 
 
-import about from "./images/about.jpg";
-import innerAbout from "./images/inner-about.jpg";
-import consultancy from "./images/consultancy.jpg";
-import attorneysStrip from "./images/attorneys-strip.jpg";
-
-import practice1 from "./images/practice-1.jpg";
-import practice2 from "./images/practice-2.jpg";
-import practice3 from "./images/practice-3.jpg";
-import practice4 from "./images/practice-4.jpg";
-
-import attorney1 from "./images/attorney-1.jpg";
-import attorney2 from "./images/attorney-2.jpg";
-import attorney3 from "./images/attorney-3.jpg";
-import attorney4 from "./images/attorney-4.jpg";
-
-import blog1 from "./images/blog-1.jpg";
-import blog2 from "./images/blog-2.jpg";
-import blog3 from "./images/blog-3.jpg";
-
-
 export const imageAssets = {
   brand:{
     logo: brandLogo,
@@ -402,26 +382,6 @@ sumathiAwards:{
 contact:{
   hero:contactHero,
 },
-
-  practice: {
-    businessLaw: practice1,
-    investmentLitigation: practice2,
-    trustsAndEstates: practice3,
-    personalInjury: practice4,
-  },
-  attorneys: {
-    attorney1,
-    attorney2,
-    attorney3,
-    attorney4,
-    strip: attorneysStrip,
-  },
-  journal: {
-    post1: blog1,
-    post2: blog2,
-    post3: blog3,
-  },
-  consultancy,
 } as const;
 
 export type ImageAssets = typeof imageAssets;
